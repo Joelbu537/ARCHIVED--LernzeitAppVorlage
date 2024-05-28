e4a13492320045ea9745ba3408fa34d0dff80e20
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2024-05-28 20:53:55 +0200

    V 0.5.5.4 Forgot to update the if(acces) loop to the new values resulting in students being unable to log in and teachers getting into the student overlay

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -48,7 +48,7 @@ namespace LernzeitApp
         public int ServerPort { get; set; }
         public AppInfo()
         {
-            Version = "0.5.5.3";
+            Version = "0.5.5.4";
             ServerIP = "127.0.0.1";
             ServerPort = 33533;
         }
